6cdaa4ead4586c6545ff6a777256af29cd6262c6
Irfan Yahya Abdillah <irfanyahyaa@gmail.com>
2024-06-13 22:16:24 +0700

    [FEATURE] add Roger.java class

--- src/main/java/com/irfan/Main.java
+++ src/main/java/com/irfan/Main.java
@@ -1,9 +1,11 @@
 package com.irfan;
 
+import static com.irfan.Roger.roger;
 import static com.irfan.Rusak.rusak;
 
 public class Main {
     public static void main(String[] args) {
         rusak(1000); // 1994
+        System.out.println(roger(8, 8)); // 4
     }
 }
---
+++ src/main/java/com/irfan/Roger.java
@@ -0,0 +1,9 @@
+package com.irfan;
+
+public class Roger {
+    public static int roger(int k, int b) {
+        if (b == 1) return 1;
+        else if (k % b == 0) return 1 + roger(k, --b);
+        else return 0 + roger(k, --b);
+    }
+}
